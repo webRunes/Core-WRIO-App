991ff720aabb9613aa11285bebabe5daa56fb011
michbil <denso.ffff@gmail.com>
2015-05-13 23:23:21 +0300

    initial nodejs setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+var nconf = require("./wrio_nconf.js").init();
 var express = require('express');
 var app = require("./wrio_app.js").init(express);
-var server = require('http').createServer(app).listen(1234);
+var server = require('http').createServer(app).listen(nconf.get("server:port"));
 
-var nconf = require("./wrio_nconf.js").init();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+
+    response.render('core.ejs',{});
+
+});
