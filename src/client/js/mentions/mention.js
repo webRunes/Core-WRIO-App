21568f567b789fc676d879cb234f11a54077255d
Michael Bilenko <denso.ffff@gmail.com>
2016-01-25 18:45:33 +0200

    Add lint support to login

--- src/client/js/mentions/mention.js
+++ src/client/js/mentions/mention.js
@@ -2,45 +2,45 @@ import sortBy from 'lodash.sortby';
 
 export class Mention {
 
-	constructor(opts) {
-		this.name = opts.name;
-		this.url = opts.url;
-		var cutUrl = this.url.split('\''),
-			positions = cutUrl[2].replace(':', '').split(',');
-		this.linkWord = cutUrl[1];
-		this.newUrl = cutUrl[0] + this.name.replace(/\s/g, '-');
-		this.order = Number(positions[0]);
-		this.start = Number(positions[1]);
-	}
+    constructor(opts) {
+        this.name = opts.name;
+        this.url = opts.url;
+        var cutUrl = this.url.split('\''),
+            positions = cutUrl[2].replace(':', '').split(',');
+        this.linkWord = cutUrl[1];
+        this.newUrl = cutUrl[0] + this.name.replace(/\s/g, '-');
+        this.order = Number(positions[0]);
+        this.start = Number(positions[1]);
+    }
 
-	warn(text) {
-		text = text || 'Wrong mention: ' + this.url;
-	}
+    warn(text) {
+        text = text || 'Wrong mention: ' + this.url;
+    }
 
-	attach(s) {
-		var before = s.substr(0, this.start),
-			toReplace = s.substr(this.start, this.linkWord.length),
-			after = s.substring(this.start + this.linkWord.length, s.length);
-		if (toReplace === this.linkWord) {
-			return {
-				before: before,
-				link: {
-					text: toReplace,
-					url: this.newUrl
-				},
-				after: after
-			};
-		}
-		this.warn();
-		return {
-			before: s
-		};
-	}
+    attach(s) {
+        var before = s.substr(0, this.start),
+            toReplace = s.substr(this.start, this.linkWord.length),
+            after = s.substring(this.start + this.linkWord.length, s.length);
+        if (toReplace === this.linkWord) {
+            return {
+                before: before,
+                link: {
+                    text: toReplace,
+                    url: this.newUrl
+                },
+                after: after
+            };
+        }
+        this.warn();
+        return {
+            before: s
+        };
+    }
 }
 
 export function merge(mentions) {
-	return sortBy(mentions, (m) => {
-		var mention = new Mention(m);
-		return mention.order;
-	});
+    return sortBy(mentions, (m) => {
+        var mention = new Mention(m);
+        return mention.order;
+    });
 }
