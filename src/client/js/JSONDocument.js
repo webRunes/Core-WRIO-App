42d8e66d32c80f0043692b7096da51fb4a23ac45
Michael Bilenko <denso.ffff@gmail.com>
2016-10-01 12:46:01 +0300

    Add loading progress indicator

--- src/client/js/JSONDocument.js
+++ src/client/js/JSONDocument.js
@@ -6,6 +6,7 @@ import {extractMentions} from './mentions/mention';
 import Immutable from 'immutable';
 import {ContentBlock, CharacterMetadata, Entity} from 'draft-js';
 
+
 var cleshe = '<!DOCTYPE html><html><head><meta charset="utf-8">' +
     '<meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">' +
     '<noscript><meta http-equiv="refresh" content="0; URL=https://wrioos.com/no_jscript.html"></noscript>' +
