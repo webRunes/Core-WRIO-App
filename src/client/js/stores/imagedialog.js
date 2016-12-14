da8c90005d3abe13650bc0768fe4c026696a17c5
Michael Bilenko <denso.ffff@gmail.com>
2016-12-14 16:58:23 +0200

    Extract title and description from Social Media

--- src/client/js/stores/imagedialog.js
+++ src/client/js/stores/imagedialog.js
@@ -4,6 +4,29 @@
 import Reflux from 'reflux';
 import LinkActions from '../actions/imagedialog.js';
 import TextEditorActions from '../actions/texteditor.js';
+import request from 'superagent';
+
+const IFRAMELY_LIMIT = 5000; // limit period between iframely requests to 5s
+
+const currentTime = () => new Date().getTime();
+const isHitTimeout = () => (currentTime() - last_time) < IFRAMELY_LIMIT;
+let last_time = 0;
+let activeTimeout = false;
+const checkTimeout = () => new Promise((resolve,reject) => {
+    if (activeTimeout) {
+        reject("too often");
+    }
+    if (isHitTimeout()) {
+        activeTimeout = true;
+        setTimeout(()=>{
+            activeTimeout = false;
+            resolve();
+        },IFRAMELY_LIMIT)
+    } else {
+        resolve();
+    }
+    last_time = currentTime();
+});
 
 export default Reflux.createStore({
     listenables: LinkActions,
@@ -66,6 +89,29 @@ export default Reflux.createStore({
     onUrlChange(val) {
         this.state.urlValue = val;
         this.trigger(this.state);
+        this.onRequestIframelyPreview(val);
     },
+
+    onRequestIframelyPreview(url) {
+        checkTimeout().then(()=>{
+            return this.downloadEmebed(url);
+        }).then((parsedData)=>{
+               this.onDescChange(parsedData.description);
+               this.onTitleChange(parsedData.title);
+        }).catch((err)=> {
+              console.log(err);
+        });
+    },
+
+    downloadEmebed(url) {
+        return new Promise((resolve, reject) => {
+            request.get('https://iframely.wrioos.com/iframely?url=' + url, (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(result.body.meta);
+            });
+        });
+    }
 
 });
