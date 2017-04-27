5fa5d5435be7cd40b7c2c3226553b7e8558b176a
Michael Bilenko <denso.ffff@gmail.com>
2017-04-27 17:58:29 +0300

    FIx lint

--- src/client/js/EditorEntities/SocialMediaEntity.js
+++ src/client/js/EditorEntities/SocialMediaEntity.js
@@ -100,9 +100,9 @@ export default class SocialMediaEntity extends React.Component {
         const content = this.getContent();
         const title = this.state.title;
         const description= this.state.description;
-        return <div onClick={this.onLinkEdit}>
+        return (<div onClick={this.onLinkEdit}>
                 <Figure content={content} title={title} description={description}/>
-            </div>;
+            </div>);
     }
 }
 
