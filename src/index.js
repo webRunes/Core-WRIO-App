d6327894353f307394686397fc0dcc96e635a702
Michael Bilenko <denso.ffff@gmail.com>
2017-06-01 09:53:40 +0300

    Move front-end code to WRIO-InternetOS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs =require('fs');
-const ejs =require('ejs');
-const session =require('express-session');
-const cookieParser =require('cookie-parser');
-const _SessionStore =require('connect-mongo');
 const nconf = require('./server/wrio_nconf.js');
 
 var app = express();
@@ -21,7 +17,6 @@ var server = require('http')
         console.log("Application Started!");
     });
 
-
 app.get('/create', (request, response) => {
     response.sendFile(__dirname +
         '/client/views/core.html');
@@ -31,34 +26,5 @@ app.get('/edit', (request, response) => {
     response.sendFile(__dirname +
         '/client/views/core.html');
 });
-
-function setupDevServer () {
-    console.log("Devserv start");
-    const webpack = require('webpack');
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-
-    let config = require('../webpack.config');
-    config.output.filename = 'client.js'; //override output filename
-    config.output.path = '/';
-    const compiler = webpack(config);
-
-
-    app.use(webpackDevMiddleware(compiler,{
-        publicPath: "/assets/",
-        stats: {colors: true},
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: true
-        }
-    }));
-}
-
-if (nconf.get("db:workdomain") === '.wrioos.local') {
-    setTimeout(() => console.log("Building in progress, sit tight... ☕ ☕ ☕"),2000);
-    setupDevServer();
-} else {
-    console.log("Production hosting mode");
-    app.use('/assets', express.static(path.join(__dirname, "..", './app/client')));
-}
 
 module.exports =  app;
